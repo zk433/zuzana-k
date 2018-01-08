1ad5bc2314a34e710ea60683d278c6a471c93d5e
Zuzana K <zk0433@gmail.com>
2018-01-08 14:19:14 +0000

    windows scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Toggle open and close nav styles on click
 
     // Toggle open and close nav styles on click
     $('#nav-toggle').click(function() {
@@ -47,39 +46,3 @@ $(document).ready(function() {
         }
     });
 });
-
-//retro clock function
-/*
-var hourHand = document.querySelector('.hours'),
-minuteHand = document.querySelector('.minutes'),
-secondHand = document.querySelector('.seconds');
-
-
-function localTime() {
-  var currentTime = new Date();
-  
-    // Seconds
-  var seconds = currentTime.getSeconds();
-  
-  var secondDeg = ((seconds/60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondDeg}deg)`;
-
-  // Minutes
-  var minutes = currentTime.getMinutes();
-  
-  var minuteDeg = ((minutes / 60) * 360) + ((seconds/60)*6);
-  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-  
-  
-  // Hours
-  var hours = currentTime.getHours();
-  
-  var hourDeg = ((hours/12)*360) + ((minutes/60) * 30);
-    hourHand.style.transform = `rotate(${hourDeg}deg)`; 
-    
-} // end of localTime function
-
-setInterval(localTime, 1000);
-
-localTime();
-*/
